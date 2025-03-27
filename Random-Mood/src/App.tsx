7c7d8fc78aa418c0c676724042786812bd56cb3a
Oğuz Geylani Yılmaz <oguzgeylaniyilmaz@gmail.com>
2025-03-27 14:02:05 +0100

    Random-mood

---
+++ Random-Mood/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Kawaii from "./components/Kawaii/Kawaii";
+
+function App() {
+  return (
+    <>
+      <Kawaii />
+    </>
+  );
+}
+
+export default App;
---
+++ Random-Mood/src/components/Kawaii/Kawaii.tsx
@@ -0,0 +1,37 @@
+import { Planet } from "react-kawaii";
+import { useState } from "react";
+
+type KawaiiMood =
+  | "sad"
+  | "happy"
+  | "blissful"
+  | "lovestruck"
+  | "excited"
+  | "ko";
+
+const moods: KawaiiMood[] = [
+  "sad",
+  "happy",
+  "blissful",
+  "lovestruck",
+  "excited",
+  "ko",
+];
+
+function App() {
+  const [mood, setMood] = useState<KawaiiMood>("happy");
+
+  const handleClick = () => {
+    const randomMood = moods[Math.floor(Math.random() * moods.length)];
+    setMood(randomMood);
+  };
+
+  return (
+    <>
+      <Planet size={300} mood={mood} color="#FDA7DC" />
+      <button onClick={handleClick}>Random Mood</button>
+    </>
+  );
+}
+
+export default App;
