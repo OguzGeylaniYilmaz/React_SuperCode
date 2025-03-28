f3a8ac6c2720a3140a47bfc450817abd995ec87c
Oğuz Geylani Yılmaz <oguzgeylaniyilmaz@gmail.com>
2025-03-28 14:23:12 +0100

    adding first-project

---
+++ Use-State/Day-Night-Change/src/App.tsx
@@ -0,0 +1,8 @@
+import "./App.css";
+import DayNightSwitch from "./components/DayNightSwitch/DayNightSwitch";
+
+function App() {
+  return <DayNightSwitch />;
+}
+
+export default App;
---
+++ Use-State/Day-Night-Change/src/components/DayNightSwitch/DayNightSwitch.tsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import "./DayNightSwitch.css";
+
+const DayNightSwitch = () => {
+  const [isNight, setIsNight] = useState(false);
+
+  const toogleDayNight = () => {
+    setIsNight(!isNight);
+  };
+
+  return (
+    <div className={`day-night ${isNight ? "night" : "day"}`}>
+      <h1>{isNight ? "Night" : "Day"}</h1>
+      <button onClick={toogleDayNight}>
+        {isNight ? "Change to Night" : "Change to Day"}
+      </button>{" "}
+    </div>
+  );
+};
+
+export default DayNightSwitch;
