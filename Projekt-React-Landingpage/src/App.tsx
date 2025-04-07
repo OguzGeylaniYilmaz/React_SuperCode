129214635d64f74e128f17c0c1893379a7f614a9
Oğuz Geylani Yılmaz <oguzgeylaniyilmaz@gmail.com>
2025-04-07 11:08:47 +0200

    update on app.tsx

--- Projekt-React-Landingpage/src/App.tsx
+++ Projekt-React-Landingpage/src/App.tsx
@@ -3,13 +3,12 @@ import FirstSection from "./components/Firstsection/Firstsection";
 import Footer from "./components/Footer/Footer";
 import Head from "./components/Head/Head";
 
-
 function App() {
   return (
     <>
       <Head />
-      <FirstSection/>
-      <Footer/>
+      <FirstSection />
+      <Footer />
     </>
   );
 }
